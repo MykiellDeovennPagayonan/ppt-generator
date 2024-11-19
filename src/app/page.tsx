7976b7e7b2f5e1eb92085e0d55f327838f7d42a3
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2024-11-19 23:14:08 +0800

    [build] Fix build issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 'use client'
-import CreatePpt from "@/components/CreatePpt"
-import { useState } from "react"
-import { Slide } from "@/lib/types"
-import Ppt from "@/components/Ppt"
+import dynamic from 'next/dynamic';
+import { useState } from "react";
+import { Slide } from "@/lib/types";
+import Ppt from "@/components/Ppt";
+
+const CreatePpt = dynamic(() => import('@/components/CreatePpt'), { ssr: false });
 
 export default function Home() {
-  const [slides, setSlides] = useState<Slide[]>([])
+  const [slides, setSlides] = useState<Slide[]>([]);
 
   return (
     <div className="min-h-screen w-full p-16">
-      {slides.length === 0 ?
+      {slides.length === 0 ? (
         <CreatePpt setSlides={setSlides} />
-        :
+      ) : (
         <Ppt slides={slides} />
-      }
+      )}
     </div>
-  )
+  );
 }
